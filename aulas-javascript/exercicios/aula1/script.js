1583cf98f5926c1b2fde9240d4a7973cc64625f0
Rafaela Gomes <rafahh97@gmail.com>
2020-09-23 12:59:18 -0300

    add function "checarsepodedirigir"

---
+++ aulas-javascript/exercicios/aula1/script.js
@@ -0,0 +1,10 @@
+
+function checarsepodedirigir() {
+    let idade = prompt('Informe sua idade')
+    
+    if (idade >= 18){
+        alert ('Já pode dirigir!')
+    } else {
+        alert ('Ainda não pode dirigir!')
+    }
+}
