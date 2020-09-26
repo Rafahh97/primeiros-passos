3a1b8a7d2664d1e551101ebc85b10e48eab24b8e
Rafaela Gomes <rafahh97@gmail.com>
2020-09-26 13:47:54 -0300

    fix color and text problems

--- aulas-javascript/exercicios/aula2/script.js
+++ aulas-javascript/exercicios/aula2/script.js
@@ -1,17 +1,24 @@
 function saoDiferentes(){
-    document.querySelector('numero1').valueAsNumber;
-    document.querySelector('numero2').valueAsNumber;
-    if (numero1 =! numero2){
+    let numero1 = document.querySelector('#numero1').value;
+    let numero2 = document.querySelector('#numero2').value;
+    if (numero1 != numero2){
         let div = document.createElement('div');
         div.innerText = 'Os valores são diferentes';
         document.body.append(div); 
+        div.classList.add('verde');
+        let texto = document.querySelector(".verde")
         texto.style.color = 'green';
+        
     }else{
         let div = document.createElement('div');
         div.innerText = 'Não são diferentes';
         document.body.append(div); 
+        div.classList.add('vermelho');
+        let texto = document.querySelector(".vermelho")
         texto.style.color = 'red';
+        
     }
+    
 }
 function resetar(){
     document.getElementById('numero1').value='';
@@ -19,5 +26,5 @@ function resetar(){
     document.getElementById('os valores são diferestes, não são diferentes').remove();
 }
 
-document.getElementById('button1').onclick = function(saoDiferentes){}
-document.getElementById('button2').onclick = function(resetar){}
+document.getElementById('button1').onclick = saoDiferentes;
+document.getElementById('button2').onclick = resetar;
